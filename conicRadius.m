b9eacae0267621df2f521e810d0fb57e9af71898
Hakim Lachnani <43235050+lachnani@users.noreply.github.com>
2019-09-03 19:48:36 -0700

    Delete conicRadius.m

--- conicRadius.m
+++
@@ -1,8 +0,0 @@
-function r = conicRadius( rp, ecc, theta )
-%CONICRADIUS Answer the radius of a conic section
-%with periapsis radius rp, eccentricity ecc, 
-%at true anomaly theta (degrees).
-
-r = rp*( 1 + ecc) ./ ( 1 + ecc*cosd(theta) ); % Polar equation
-end
-
